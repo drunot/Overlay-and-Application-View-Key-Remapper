f0b18e3e1a39e4d3df860fc019689302cbb0bf29
Anton Vigen Smolarz <anton@smolarz.dk>
2025-06-14 17:58:24 +0200

    Shortcut is now working.

---
+++ app-window-view-key-remapper@antonvigensmolarz.dk/extension.js
@@ -0,0 +1,53 @@
+import {
+  Extension,
+  gettext as _,
+} from "resource:///org/gnome/shell/extensions/extension.js";
+import * as Overview from "resource:///org/gnome/shell/ui/overview.js";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+
+import Shell from "gi://Shell";
+import Meta from "gi://Meta";
+
+export default class StartOverlayInAppViewExtension extends Extension {
+  originalToggle;
+
+  constructor(metadata) {
+    super(metadata);
+
+    this.originalToggle = Overview.Overview.prototype.toggle;
+  }
+
+  _toggle_overview() {
+    if (Main.overview.visible) {
+      Main.overview.hide();
+    } else {
+      Main.overview.show();
+    }
+  }
+
+  enable() {
+    this._settings = this.getSettings();
+
+    Overview.Overview.prototype.toggle = function () {
+      if (this.isDummy) return;
+
+      if (this._visible) this.hide();
+      else this.showApps();
+    };
+
+    Main.wm.addKeybinding(
+      "remapper-toggle-overview-key",
+      this._settings,
+      Meta.KeyBindingFlags.NONE,
+      Shell.ActionMode
+        ? Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW
+        : Shell.KeyBindingMode.NORMAL | Shell.KeyBindingMode.OVERVIEW,
+      this._toggle_overview.bind(this)
+    );
+  }
+
+  disable() {
+    Overview.Overview.prototype.toggle = this.originalToggle;
+    Main.wm.removeKeybinding("remapper-toggle-overview-key");
+  }
+}
